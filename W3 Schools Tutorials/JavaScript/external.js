6cc45a495d27c24391cc1a0e94dbf18f01faf4f5
Noorulhuda Usman <155479924+Noorulhuda-Usman@users.noreply.github.com>
2024-10-21 16:15:26 +0500

    Add W3Schools JavaScript tutorials

---
+++ W3 Schools Tutorials/JavaScript/external.js
@@ -0,0 +1,8 @@
+// this method uses a file path to link to myScript.js
+<script src="/js/myScript.js"></script>
+
+// this method uses a full URL to link to myScript.js
+// <script src="https://www.w3schools.com/js/myScript.js"></script>
+
+// This example uses no path to link to myScript.js
+// <script src="myScript.js"></script>
